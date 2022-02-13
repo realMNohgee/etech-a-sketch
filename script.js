100869e73297259d2d6b9bd7dbf4d1261e10f037
realMNohgee <clund25@gmail.com>
2022-02-13 15:46:38 -0600

    adding to JS file added getGrid() function

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
+// Setting constants and variables //
+
 const container = document.getElementById('container');
 let rows = document.getElementsByClassName('gridRows');
 let cells = document.getElementsByClassName('cell');
+
+// Create default gird size //
+
+function getGrid() {
+    makeRows (16);
+    makeColumns(16);
+}
+
+function makeRows (numberOf) {
+    for (r = 0; r < numberOf; r++){
+        let row = document.createElement('div');
+        container.appendChild(row).className = 'gridRows';
+    };
+};
+
+// Create columns //
 
